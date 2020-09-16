be531ccaa8e2249bc20a012cab112b50028e72ee
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-09-16 06:47:53 -0300

    add react

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,15 @@
-import "./scss/main.scss";
-import "./js/main.js";
+import './scss/main.scss';
+import 'lazysizes';
+import './js/App.js';
+
+//import React from "react";
+// import ReactDOM from "react-dom";
+// import MyAmazingComponent from "./modules/MyAmazingComponent";
+// ReactDOM.render(
+//   <MyAmazingComponent />,
+//   document.querySelector("#my-react-example")
+// );
+
+if (module.hot) {
+	module.hot.accept();
+}
