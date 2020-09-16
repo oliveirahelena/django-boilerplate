440ae8b959fcd9855162ee94caa69cb2f9374e1b
Helena Oliveira <helena.oliveira@casaldev.com.br>
2020-09-16 15:10:26 -0300

    add allauth

--- demo/settings/base.py
+++ demo/settings/base.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 ALLOWED_HOSTS = []
+SITE_ID = 1
 
 # Application definition
 
@@ -19,13 +20,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # Third party
-    'corsheaders',
-    'webpack_loader',
-    'storages',
     'rest_framework',
     'rest_framework.authtoken',
     'dj_rest_auth',
+    'allauth',
+    'allauth.account',
+    'dj_rest_auth.registration',
+    'allauth.socialaccount',
+    'corsheaders',
+    'webpack_loader',
+    'storages',
     # Local
     'core',
 ]
@@ -128,6 +134,15 @@ WEBPACK_LOADER = {
 # read os.environ['REDIS_URL']
 # 'redis': env.cache('REDIS_URL')
 # }
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 # Rest Framework
 
